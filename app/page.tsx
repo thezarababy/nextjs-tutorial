965080f2f2688b67f7a4ad003e3503e33aa003e5
thezarababy <jyokoduwa@gmail.com>
2026-01-01 15:55:55 +0100

    Link component

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,13 @@
-import React from "react";
+import Link from "next/link";
 
 const home = () => {
-  return <div>Welcome home !</div>;
+  return (
+    <>
+      <h1>Welcome home !</h1>
+      <Link href="/about">About</Link>
+      <Link href="/products">Product</Link>
+    </>
+  );
 };
 
 export default home;
--- app/products/page.tsx
+++ app/products/page.tsx
@@ -1,15 +1,25 @@
-import React from "react";
+import Link from "next/link";
 
 const page = () => {
+  const productID = 100;
   return (
-    <div>
+    <>
       <h1>Product list</h1>
       <ul>
-        <li>Product 1</li>
-        <li>Product 2</li>
-        <li>Product 3</li>
+        <Link href="/products/1">
+          <li>Product 1</li>
+        </Link>
+        <Link href="/products/2">
+          <li>Product 2</li>
+        </Link>
+        <Link href="/products/3">
+          <li>Product 3</li>
+        </Link>
+        <Link href={`/products/${productID}`}>
+          <li>Product {productID}</li>
+        </Link>
       </ul>
-    </div>
+    </>
   );
 };
 

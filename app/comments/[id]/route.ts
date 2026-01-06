3e2f107f9cf87455b16853e47ab0d3243cccb9ce
thezarababy <jyokoduwa@gmail.com>
2026-01-06 16:10:55 +0100

    DELETE request

--- app/comments/[id]/route.ts
+++ app/comments/[id]/route.ts
@@ -22,3 +22,13 @@ export async function PATCH(
   comments[index].text = text;
   return Response.json(comments[index]);
 }
+
+export async function DELETE(
+  request: Request,
+  { params }: { params: Promise<{ id: string }> }
+) {
+  const { id } = await params;
+  const index = comments.findIndex((comment) => comment.id === parseInt(id));
+  comments.splice(index, 1);
+  return Response.json({ message: "Comment deleted successfully" });
+}

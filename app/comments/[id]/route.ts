d90cc462a22f33d762039b0e7b3521d59c482567
thezarababy <jyokoduwa@gmail.com>
2026-01-06 16:06:42 +0100

    PATCH request

--- app/comments/[id]/route.ts
+++ app/comments/[id]/route.ts
@@ -8,3 +8,17 @@ export async function GET(
   const comment = comments.find((comment) => comment.id === parseInt(id));
   return Response.json(comment);
 }
+
+export async function PATCH(
+  request: Request,
+  { params }: { params: Promise<{ id: string }> }
+) {
+  const { id } = await params;
+  const body = await request.json();
+  const { text } = body;
+
+  const index = comments.findIndex((comment) => comment.id === parseInt(id));
+
+  comments[index].text = text;
+  return Response.json(comments[index]);
+}

a6e74988f5b64152c516db733c429c712d17df56
thezarababy <jyokoduwa@gmail.com>
2026-01-07 10:34:13 +0100

    Cookies

--- app/hello/api/route.ts
+++ app/hello/api/route.ts
@@ -1,12 +1,17 @@
 import { type NextRequest } from "next/server";
-import { headers } from "next/headers";
+import { headers, cookies } from "next/headers";
 
 export async function GET(request: NextRequest) {
   // const requestHeaders = new Headers(request.headers);
   // console.log(requestHeaders.get("Authorization"));
   const headersList = await headers();
   console.log(headersList.get("authorization"));
+  const theme = request.cookies.get("theme");
+  console.log(theme);
+  const cookieStore = await cookies();
+  cookieStore.set("resultsPerPage", "20");
+  console.log(cookieStore.get("resultsPerPage"));
   return new Response("<h1>Hello, Next.js!</h1>", {
-    headers: { "Content-Type": "text/html" },
+    headers: { "Content-Type": "text/html", "Set-Cookie": "theme=dark" },
   });
 }

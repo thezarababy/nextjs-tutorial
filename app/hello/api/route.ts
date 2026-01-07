148c1bc59e3cf969b6f574484571579141cb1901
thezarababy <jyokoduwa@gmail.com>
2026-01-07 10:20:37 +0100

    content-type for headers

--- app/hello/api/route.ts
+++ app/hello/api/route.ts
@@ -6,5 +6,7 @@ export async function GET(request: NextRequest) {
   // console.log(requestHeaders.get("Authorization"));
   const headersList = await headers();
   console.log(headersList.get("authorization"));
-  return new Response("Hello, Next.js!");
+  return new Response("<h1>Hello, Next.js!</h1>", {
+    headers: { "Content-Type": "text/html" },
+  });
 }

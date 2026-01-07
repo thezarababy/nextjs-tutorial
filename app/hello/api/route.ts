9d48bcd1c41193882ae7877d2cebe51f738474c8
thezarababy <jyokoduwa@gmail.com>
2026-01-07 10:15:32 +0100

    header request

--- app/hello/api/route.ts
+++ app/hello/api/route.ts
@@ -1,3 +1,10 @@
-export async function GET() {
+import { type NextRequest } from "next/server";
+import { headers } from "next/headers";
+
+export async function GET(request: NextRequest) {
+  // const requestHeaders = new Headers(request.headers);
+  // console.log(requestHeaders.get("Authorization"));
+  const headersList = await headers();
+  console.log(headersList.get("authorization"));
   return new Response("Hello, Next.js!");
 }

1035d940f9998c3a480a505d2fcba37a27b5126c
thezarababy <jyokoduwa@gmail.com>
2026-01-08 10:37:28 +0100

    middleware-conditional statements

--- middleware.ts
+++ middleware.ts
@@ -2,8 +2,12 @@ import { NextRequest } from "next/server";
 import { NextResponse } from "next/server";
 
 export function middleware(request: NextRequest) {
-  return NextResponse.redirect(new URL("/", request.url));
+  //   return NextResponse.redirect(new URL("/",
+  // request.url));
+  if (request.nextUrl.pathname === "/profile") {
+    return NextResponse.redirect(new URL("/", request.nextUrl));
+  }
 }
-export const config = {
-  matcher: "/profile",
-};
+// export const config = {
+//   matcher: "/profile",
+// }

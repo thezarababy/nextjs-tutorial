ee59b9845b161959c66fff282bf7fd50cfb80684
thezarababy <jyokoduwa@gmail.com>
2026-01-15 18:44:19 +0100

    Interleaving-client component inside server component

--- app/interleaving/page.tsx
+++ app/interleaving/page.tsx
@@ -1,9 +1,9 @@
-import ClientComponentOne from "@/components/client-component-one";
+import ServerComponentOne from "@/components/server-component-one";
 const Interleaving = () => {
   return (
     <div>
       <h1>Interleaving Page</h1>
-      <ClientComponentOne />
+      <ServerComponentOne />
     </div>
   );
 };
--- components/server-component-one.tsx
+++ components/server-component-one.tsx
@@ -1,12 +1,12 @@
 import fs from "fs";
-import ServerComponentTwo from "./server-component-two ";
+import ClientComponentOne from "./client-component-one";
 
 const ServerComponentOne = () => {
   fs.readFileSync("components/server-component-one.tsx", "utf-8");
   return (
     <div>
       <h1>Server Component One</h1>
-      <ServerComponentTwo />
+      <ClientComponentOne />
     </div>
   );
 };
